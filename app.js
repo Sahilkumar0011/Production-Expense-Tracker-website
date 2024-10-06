421b156b8fe95c8b03801f97bfda5cbd0744f721
Sahilkumar0011 <114636616+Sahilkumar0011@users.noreply.github.com>
2024-10-06 14:31:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const dotenv= require("dotenv")
 const colors= require("colors")
 const path= require('path')
 const connectdb = require("./config/connectdb")
-const cookieParser = require("cookie-parser")
+
 
 dotenv.config()
 
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(morgan('dev'))
 app.use(cors())
-app.use(cookieParser)
+
 
 
 // user routes
